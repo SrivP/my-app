13bfdb7b133678494894c47c7fb89d54a559ad3f
Srivathsan <srivprasanna@gmail.com>
2025-01-23 16:21:33 -0500

    added focus page, added sidebar

---
+++ app/focus/page.tsx
@@ -0,0 +1,123 @@
+"use client"
+import { AlarmClock } from 'lucide-react';
+import { Button } from "@/components/ui/button"
+import {useEffect, useState} from 'react'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import Sidebar from '@/components/custom/sidebar';
+
+
+export default function page() {
+  const [time, setTime] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [variant, setVariant] = useState<"default" | "secondary"| "destructive">("default");
+  
+
+  useEffect(() => {
+    let countdown: NodeJS.Timeout | undefined;
+
+    if (running) {
+    countdown = setInterval(() => {
+      if (running) {
+        setTime((prev) => prev - 1);
+      }
+    },1000) 
+  }
+  
+  return () => {
+    if (countdown) {
+      clearInterval(countdown);
+  }
+  
+  }
+
+  }, [running])
+
+  function handleStart() {
+    setRunning(true);
+    setVariant("destructive");
+
+  }
+
+  function formatTime(time: number) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  
+  }
+
+  function handleTime() {
+
+    setTime(minutes * 60 + seconds);
+  }
+
+  function handleReset() {
+    setTime(0);
+    setRunning(false);
+    setVariant("default");
+  }
+
+
+
+  return (
+    <div className="grid grid-cols-[1fr_5fr] h-screen">
+      <Sidebar />
+    <div className="grid place-items-center h-screen">
+    <div className="items-center  flex flex-col w-[30rem] h-[20rem] shadow-md rounded-md bg-slate-50 ">
+        <p className="pt-1 text-9xl font-mono font-extrabold">{formatTime(time)}</p>  
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button className="mt-4 ml-0" variant="ghost"><AlarmClock /></Button>
+          </PopoverTrigger>
+      <PopoverContent className="w-80">
+        <div className="grid gap-4">
+          <div className="space-y-2">
+            <h4 className="font-medium leading-none">Set Time</h4>
+            <p className="text-sm text-muted-foreground">
+              Set the amount of time you'd like to focus for (in seconds).
+            </p>
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="minutes">Minutes</Label>
+              <Input
+                id="minutes"
+                defaultValue="25"
+                className="col-span-2 h-8"
+                onChange={(e) => setMinutes(parseInt(e.target.value))}
+                
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="seconds">Seconds</Label>
+              <Input
+                id="seconds"
+                defaultValue="00"
+                className="col-span-2 h-8"
+                onChange={(e) => setSeconds(parseInt(e.target.value))}
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Button onClick={handleTime} className="col-span-2">Submit</Button>
+            </div>
+            
+            </div>
+        </div>
+      </PopoverContent>
+      </Popover>
+        <div className="mt-[45px] space-x-10">
+            <Button className="" variant={variant} onClick={handleStart}>{running ? "Stop" : "Start"}</Button> 
+            <Button className=" " variant="default" onClick={handleReset}>Reset</Button>
+        </div>
+    </div>
+    </div>
+    </div>
+  );
+}
--- components/custom/sidebar.tsx
+++ components/custom/sidebar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 export default function Sidebar() {
     return (
         <>
-            <div className="flex flex-col w-1/6 h-100vh border-r border-r-stone-300 space-y-2 justify-center items-center ">
-                <Link href="/" className="text-left hover:bg-slate-200">Focus</Link>
+            <div className="flex flex-col min-w-1/6 min-h-screen border-r border-r-stone-300 space-y-2 justify-center items-center ">
+                <Link href="/focus" className="text-left hover:bg-slate-200">Focus</Link>
                 <Link href="/project" className="hover:bg-slate-200">Projects</Link>
                 <Link href="/" className="hover:bg-slate-200">Stats</Link>
             </div>
